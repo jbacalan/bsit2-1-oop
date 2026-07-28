094552f070e1c8dcb4e6215248cd65bef8e40e26
jbacalan <jbacalan93653@liceo.edu.ph>
2026-07-28 21:45:50 +0800

    Update book.java

--- src/book.java
+++ src/book.java
@@ -1,43 +1,35 @@
 public class Book {
 
-    // Private fields
     private String title;
     private String author;
     private boolean isBorrowed;
 
-    // Constructor
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
         this.isBorrowed = false;
     }
 
-    // Getter for title
     public String getTitle() {
         return title;
     }
 
-    // Getter for author
     public String getAuthor() {
         return author;
     }
 
-    // Getter for borrowed status
     public boolean isBorrowed() {
         return isBorrowed;
     }
 
-    // Borrow the book
     public void borrow() {
         isBorrowed = true;
     }
 
-    // Return the book
     public void returnBook() {
         isBorrowed = false;
     }
 
-    // Describe the book
     public String describe() {
         String status;
 
