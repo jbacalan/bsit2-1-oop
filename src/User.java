f87c9b0596c95c954e7a082e79774f4a3ab4de0c
jbacalan <jbacalan93653@liceo.edu.ph>
2026-09-10 21:53:15 +0800

    Update User.java

--- src/User.java
+++ src/User.java
@@ -2,7 +2,7 @@
 // The ABSTRACTION. This class holds everything that is true of EVERY user.
 // It is never created directly - you can never write new User(...)
 
-public abstract class User {
+public abstract class User implements Exportable {
 
     private final int id;
     private String name;
@@ -14,21 +14,37 @@ public abstract class User {
         this.email = email;
     }
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public String getEmail() { return email; }
+    public int getId() {
+        return id;
+    }
 
-    public void setName(String name) { this.name = name; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     // TODO 4
     public abstract String role();
+
     public abstract String permissions();
 
     // TODO 5
     public void display() {
-        System.out.printf("[%d] %-12s %-24s %-8s %s%n",
-                id, name, email, role(), permissions());
+        System.out.printf(
+                "[%d] %-12s %-24s %-8s %s%n",
+                id, name, email, role(), permissions()
+        );
     }
 
     // TODO 6
@@ -37,4 +53,4 @@ public abstract class User {
         return id + "," + name + "," + email + "," + role();
     }
 }
-
+```
