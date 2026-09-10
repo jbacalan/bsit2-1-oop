8dc076e919e04e9e4b3259d8343bee74ab319668
jbacalan <jbacalan93653@liceo.edu.ph>
2026-09-10 09:15:48 +0800

    Define abstract User class with properties and methods

--- src/User.java
+++ src/User.java
@@ -1,1 +1,40 @@
+// User.java
+// The ABSTRACTION. This class holds everything that is true of EVERY user.
+// It is never created directly - you can never write new User(...)
+
+public abstract class User {
+
+    private final int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+
+    // TODO 4
+    public abstract String role();
+    public abstract String permissions();
+
+    // TODO 5
+    public void display() {
+        System.out.printf("[%d] %-12s %-24s %-8s %s%n",
+                id, name, email, role(), permissions());
+    }
+
+    // TODO 6
+    @Override
+    public String toCsv() {
+        return id + "," + name + "," + email + "," + role();
+    }
+}
 
