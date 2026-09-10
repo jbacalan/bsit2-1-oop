141814c88bed5ee3ec9fabc7877c6c51b41b597d
jbacalan <jbacalan93653@liceo.edu.ph>
2026-09-10 09:15:23 +0800

    Update Exportable.java

--- src/Exportable.java
+++ src/Exportable.java
@@ -1,1 +1,16 @@
+// Exportable.java
+// An INTERFACE = a list of promises. It says WHAT, never HOW.
+//
+// Any class that implements Exportable promises it can turn itself
+// into one line of CSV text, for example: 1,Razz,razz@liceo.edu.ph,ADMIN
+public interface Exportable {
+
+    // TODO 1
+    String toCsv();
+
+    // TODO 2
+    default void printExport() {
+        System.out.println(toCsv());
+    }
+}
 
