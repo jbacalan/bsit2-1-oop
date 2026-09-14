3e5aae6acd3ed29387885b43026ee31d56cdee21
jbacalan <jbacalan93653@liceo.edu.ph>
2026-09-14 09:03:04 +0800

    Add Refundable interface with refund method

--- src/Refundable.java
+++ src/Refundable.java
@@ -1,1 +1,13 @@
+// Refundable.java
+// An INTERFACE = a list of promises. It says WHAT, never HOW.
 
+public interface Refundable {
+
+    // TODO 1
+    String refund();
+
+    // TODO 2
+    default void printRefundNotice() {
+        System.out.println("   " + refund());
+    }
+}
