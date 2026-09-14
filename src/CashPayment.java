01c41e258935ccb5641ffec6e2b0458444d8ff37
jbacalan <jbacalan93653@liceo.edu.ph>
2026-09-14 09:04:28 +0800

    Update CashPayment.java

--- src/CashPayment.java
+++ src/CashPayment.java
@@ -1,1 +1,23 @@
+// CashPayment.java
+// A CHILD of Payment that is NOT Refundable.
 
+public class CashPayment extends Payment {
+
+    // TODO 9(a)
+    public CashPayment(int id, String payerName, double amount) {
+        super(id, payerName, amount);
+    }
+
+    // TODO 9(b)
+    @Override
+    public String provider() {
+        return "CASH";
+    }
+
+    // TODO 9(c)
+    @Override
+    public void pay() {
+        System.out.printf(" Cash: PHP %.2f received. Please get your change.%n",
+                getAmount());
+    }
+}
