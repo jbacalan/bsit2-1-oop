b870b40f5111093a88086edcaf826f29fa55a377
jbacalan <jbacalan93653@liceo.edu.ph>
2026-09-24 09:05:43 +0800

    Update Taxi.java

--- src/Taxi.java
+++ src/Taxi.java
@@ -1,1 +1,16 @@
+package semifinal;
 
+/**
+ * StudentDiscount - an INTERFACE (a list of promises).
+ * Only rides that give students a discount implement this.
+ */
+public interface StudentDiscount {
+
+    double discountedFare();
+
+    // GIVEN: a default method. Every class that implements this
+    // interface gets it for free. Notice it calls discountedFare().
+    default void printDiscount() {
+        System.out.printf("  Student fare   : PHP %.2f%n", discountedFare());
+    }
+}
